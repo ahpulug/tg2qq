8363dd2765df4966ec51a935386baef50956a467
sweet-st <foreverofsweet@gmail.com>
2017-12-12 08:16:02 +0800

    qq bot

---
+++ qqbot/__main__.py
@@ -0,0 +1,10 @@
+import sys
+import os
+
+print("pwkqkw")
+p = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+if p not in sys.path:
+    sys.path.insert(0, p)
+
+from qqbot import Main
+Main()
